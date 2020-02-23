8f83dacb1302a0a848199b26b88b57baffb76b96
Marina-Poprotskaya <60131955+Marina-Poprotskaya@users.noreply.github.com>
2020-02-23 19:37:20 +0300

    feat: solve typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array !== undefined && array.length !== 0) {
+    let number = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < number) number = array[i];
+    }
+    return number;
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+
+exports.max = function max(array) {
+  let number = 0;
+  if (array !== undefined && array.length !== 0) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > number) number = array[i];
+    }
+    return number;
+  }
   return 0;
 }
 
-exports.avg = function avg (array) {
+
+exports.avg = function avg(array) {
+  let number = 0;
+  let q = 0;
+  sum = 0;
+  if (array !== undefined && array.length !== 0) {
+    for (let i = 0; i < array.length; i++) {
+      q++;
+      sum += array[i];
+      number = sum / q;
+    }
+    return number;
+  }
   return 0;
 }
+
